2f83bea9e0219f756ef13b4dd57689aca54c01c7
pavanakumarn <126378290+pavanakumarn@users.noreply.github.com>
2023-12-04 05:12:38 +0000

    u

---
+++ Python/str.py
@@ -0,0 +1,9 @@
+str1 = "Hello"
+str2 = "World"
+result = str1 +" "+ str2
+print(result)
+
+length = len(result)
+
+print(length)
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pylearn
-
+/home/codespace/.python/current/bin/python3 /workspaces/pylearn/Python/str.py
